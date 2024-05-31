1aa25981150ebdddf66d5f770f07b04f2446b35a
刘涛 <>
2024-05-31 10:32:22 +0800

    修改os指标定时任务

--- service/schedule/index.js
+++ service/schedule/index.js
@@ -6,7 +6,21 @@ module.exports = {
     job: null,
     startMission: function(client){
       this.job = schedule.scheduleJob('*/10 * * * * *', async() => {
-        const exec_res = await HttpService.__osTargetSyncService();
+        const socket_status = (!global.socketConnectionStatus) ? 0 : 1
+        const [cpu, disk, memory, network_status] = await Promise.all([
+          basicCommand.cpuFree(),
+          basicCommand.diskFree(),
+          basicCommand.memoryFree(),
+          basicCommand.verifyNetwork()
+        ])
+        const exec_res = {
+          cpu, 
+          disk, 
+          memory, 
+          network_status, 
+          socket_status, 
+          heart_beat: global.socketHeartBeat
+        }
         EmitEvent.emitOsTargetSyncSocket(client, exec_res);
       })
     },
