0ce05dfcc9fcd6affcb5711172cb30e37bdbd0dd
刘涛 <>
2024-06-03 11:33:59 +0800

    添加打印

--- service/index.js
+++ service/index.js
@@ -163,9 +163,12 @@ class HttpService extends BaseServer {
    */
   static async __vmnicTargetSyncService() {
     const vmnic_count = basicCommand.vmnicCount();
+    console.log('vmnic_count===>>', vmnic_count);
     const data_array = [];
     for(let i = 0 ; i < vmnic_count.length; i++) {
+      console.log('i====>>', i);
       const vmnic_type = await interfaceCommand.interfaceType(`GE${i}`);
+      console.log('vmnic_type====>>', vmnic_type, vmnic_type == "WAN")
       if(vmnic_type == "WAN") {
         const [access_type, pppoe_account, ip, dns, gateway] = await Promise.all([
           interfaceCommand.interfaceMethod(`GE${i}`),
@@ -181,6 +184,7 @@ class HttpService extends BaseServer {
         data_array.push({interface_name: `GE${i}`, interface_type: 1, exec_body: {deploy_access_type: deploy_access_type, deploy_detail: deploy_detail}})
       }
     }
+    console.log('data_array===>>', data_array)
     const [ip, dns, gateway] = await Promise.all([
       basicCommand.networkAddress(`LAN`),
       basicCommand.networkDns(`LAN`),
