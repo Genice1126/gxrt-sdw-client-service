c468d84c56a747254e202613e01a3f075c13ddd0
刘涛 <>
2024-06-06 09:55:03 +0800

    修改trace命令

--- extend/command/diagnose.js
+++ extend/command/diagnose.js
@@ -15,7 +15,7 @@ exports.diagnoseAddPing = (host, address_type, interface_name, frequency = 5, in
 
 exports.diagnoseAddTraceRouter = (host, address_type, interface_name, hop_count = 18, is_as = false, is_icmp = false) => {
   return new Promise((resolve, rejected) => {
-    let query_res = `traceroute ${host} -m ${hop_count}`;
+    let query_res = `traceroute -n ${host} -m ${hop_count}`;
     (address_type == 1) ? query_res += ` -4` : query_res += ` -6`;
     if(interface_name) query_res += ` -i ${interface_name}`;
     if(is_as) query_res += ` -A`
