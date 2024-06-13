125da8c45fb9fba459bbb4ab8995b97d0ea536da
刘涛 <>
2024-06-13 14:24:55 +0800

    修改bug

--- service/schedule/index.js
+++ service/schedule/index.js
@@ -2,6 +2,7 @@ const schedule = require('node-schedule');
 const EmitEvent = require('../websocket/emit-event');
 const {basicCommand, diagnoseCommand} = require('../../extend/command');
 const Helper = require('../../extend/helper');
+const CONFIG = require('../../config/index');
 module.exports = {
   osTargetScheduleTask: {
     job: null,
@@ -43,11 +44,10 @@ module.exports = {
     }
   },
   diagnoseLinkScheduleTask: {
-    dir:'./diagnose-link',
     count: {},
     jobs: {},
     startAssignMission: async function(client, file_name) {
-      const file_ctx = await Helper.readFiles(this.dir, file_name);
+      const file_ctx = await Helper.readFiles(CONFIG.SCHEDULE_DIAGNOSE_LINK_PATH, file_name);
       console.log('file_ctx===>', file_ctx);
       const file_json = JSON.parse(file_ctx);
       const schedule_time = (file_json.diagnose_link_interval == '60') ? '* * * * *' : `*/${file_json.diagnose_link_interval} * * * * *`;
@@ -85,10 +85,10 @@ module.exports = {
       })
     },
     startAllMission: async function(client) {
-      const file_name_gather = await Helper.readDir(this.dir);
+      const file_name_gather = await Helper.readDir(CONFIG.SCHEDULE_DIAGNOSE_LINK_PATH);
       if(file_name_gather.length == 0) return;
       for(let i = 0 ; i < file_name_gather.length; i++) {
-        const file_ctx = await Helper.readFiles(this.dir, file_name_gather[i]);
+        const file_ctx = await Helper.readFiles(CONFIG.SCHEDULE_DIAGNOSE_LINK_PATH, file_name_gather[i]);
         const file_json = JSON.parse(file_ctx);
         const schedule_time = (file_json.diagnose_link_interval == '60') ? '* * * * *' : `*/${file_json.diagnose_link_interval} * * * * *`;
         this.count[file_json.interface_name] = 0;
