813060bef364e727c2b4fa20847b4d4ea3a91b1e
刘涛 <>
2024-07-16 17:51:42 +0800

    修改自组网命令

--- extend/command/manet.js
+++ extend/command/manet.js
@@ -61,7 +61,7 @@ exports.manetCreateBranch = (private_key, address_type, address, master, con_nam
   return new Promise((resolve, rejected) => {
     process.exec(`nmcli connection add type wireguard con-name ${con_name} ifname ${con_name} autoconnect yes`, (err, stdout, stderr) => {
       //ipv4.never-default yes
-      process.exec(`nmcli connection modify ${con_name} ${address_type}.method manual ${address_type}.addresses ${address} ipv4.gateway 172.31.255.1 ${address_type}.route-metric 999`, (err, stdout, stderr) => {
+      process.exec(`nmcli connection modify ${con_name} ${address_type}.method manual ${address_type}.addresses ${address} ipv4.gateway 172.31.255.1 ${address_type}.route-metric 999 wireguard.ip4-auto-default-route false`, (err, stdout, stderr) => {
         process.exec(`nmcli connection modify ${con_name} wireguard.private-key "${private_key}"`, async (err, stdout, stderr) => {
           let tmp_string = `[wireguard-peer.${master.manet_public_key}]\nendpoint=${master.address}:${master.address_port}\nallowed-ips=0.0.0.0/0\npersistent-keepalive=20\n`;
           await Helper.appendFiles('/etc/NetworkManager/system-connections/', `${con_name}.nmconnection`, `\n${tmp_string}`);
@@ -80,7 +80,7 @@ exports.manetCreateBranch = (private_key, address_type, address, master, con_nam
 /**
  * 自组网删除分支节点
  */
-exports.manetDeleteBranch = (con_name = "GE0") => {
+exports.manetDeleteBranch = (con_name = "wg0") => {
   return new Promise((resolve, rejected) => {
     process.exec(`iptables -t nat -D POSTROUTING -o ${con_name} -j MASQUERADE`, (err, stdout, stderr) => {
       process.exec(`netfilter-persistent save`, (err, stdout, stderr) => {
