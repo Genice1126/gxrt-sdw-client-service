db8ea2798a3e7e8059bdcb98ec1b53998e5ef054
刘涛 <>
2025-03-13 16:20:51 +0800

    fix

--- service/websocket/index.js
+++ service/websocket/index.js
@@ -8,8 +8,11 @@ module.exports = (socketAddress, condition) => {
   }
   condition = Object.assign(condition, {
     transports: ['websocket'],
+    pingInterval: 25000,  // 每 25 秒发送一次 ping
+    pingTimeout: 10000,   // 如果 10 秒内没有响应 pong，则认为超时
     reconnection: true, 
     reconnectionDelay: 5000,
+    reconnectionAttempts: 5,  // 最多尝试 5 次重连
   })
   global.socketConnection = io(socketAddress, condition);
   Object.keys(mapEvent).forEach(key => mapEvent[key](global.socketConnection))
