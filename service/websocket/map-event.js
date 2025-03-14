b1b30bab780c864b11b454c7eedc233a889756b4
刘涛 <>
2025-03-14 10:19:48 +0800

    no message

--- service/websocket/map-event.js
+++ service/websocket/map-event.js
@@ -18,16 +18,16 @@ exports.connection = (client) => {
   })
 }
 //监听pong
-// exports.listenPing = (client) => {
-//   client.io.engine.on("packet", (packet) => {
-//     if (packet.type === "ping") {
-//       console.log("收到服务器的 ping");
-//     }
-//     if (packet.type === "pong") {
-//       console.log("客户端发送了 pong");
-//     }
-//   });
-// }
+exports.listenPing = (client) => {
+  client.io.engine.on("packet", (packet) => {
+    if (packet.type === "ping") {
+      console.log("收到服务器的 ping");
+    }
+    if (packet.type === "pong") {
+      console.log("客户端发送了 pong");
+    }
+  });
+}
 
 //监听心跳时间事件
 exports.heartBeat = (client) => {
