1596bb718e896874f3e95042e7c281fb52412c9d
Genice <>
2026-06-24 10:39:05 +0800

    fix

--- extend/command/manet.js
+++ extend/command/manet.js
@@ -10,16 +10,18 @@ exports.manetCreateMaster = (manet_private_key, address_type, address, address_p
     process.exec(`nmcli connection add type wireguard con-name ${con_name} ifname ${con_name} autoconnect yes`, (err, stdout, stderr) => {
       process.exec(`nmcli connection modify ${con_name} ${address_type}.method manual ${address_type}.addresses ${address} ipv4.never-default yes`, (err, stdout, stderr) => {
         process.exec(`nmcli connection modify ${con_name} wireguard.private-key "${manet_private_key}"`, (err, stdout, stderr) => {
-          process.exec(`nmcli connection modify ${con_name} wireguard.listen-port ${address_port}`, async(err, stdout, stderr) => {
-            let tmp_string = '';
-            branch.map(item => tmp_string += `[wireguard-peer.${item.manet_public_key}]\nallowed-ips=${item.address}\n`)
-            await Helper.appendFiles('/etc/NetworkManager/system-connections/', `${con_name}.nmconnection`, `\n${tmp_string}`);
-            process.exec(`nmcli connection load /etc/NetworkManager/system-connections/${con_name}.nmconnection`, (err, stdout, stderr) => {
-              process.exec(`nmcli connection up ${con_name}`, (err, stdout, stderr) => {
-                process.exec(`iptables -t nat -A POSTROUTING -o ${con_name} -j MASQUERADE`, (err, stdout, stderr) => {
-                  process.exec(`netfilter-persistent save`, (err, stdout, stderr) => resolve())
-                })
-              });
+          process.exec(`nmcli connection modify ${con_name} wireguard.listen-port ${address_port}`, (err, stdout, stderr) => {
+            process.exec(`nmcli connection modify ${con_name} wireguard.mtu 1420`, async (err, stdout, stderr) => {
+              let tmp_string = '';
+              branch.map(item => tmp_string += `[wireguard-peer.${item.manet_public_key}]\nallowed-ips=${item.address}\n`)
+              await Helper.appendFiles('/etc/NetworkManager/system-connections/', `${con_name}.nmconnection`, `\n${tmp_string}`);
+              process.exec(`nmcli connection load /etc/NetworkManager/system-connections/${con_name}.nmconnection`, (err, stdout, stderr) => {
+                process.exec(`nmcli connection up ${con_name}`, (err, stdout, stderr) => {
+                  process.exec(`iptables -t nat -A POSTROUTING -o ${con_name} -j MASQUERADE`, (err, stdout, stderr) => {
+                    process.exec(`netfilter-persistent save`, (err, stdout, stderr) => resolve())
+                  })
+                });
+              })
             })
           })
         })
@@ -62,15 +64,17 @@ exports.manetCreateBranch = (private_key, address_type, address, master, con_nam
     process.exec(`nmcli connection add type wireguard con-name ${con_name} ifname ${con_name} autoconnect yes`, (err, stdout, stderr) => {
       //ipv4.never-default yes
       process.exec(`nmcli connection modify ${con_name} ${address_type}.method manual ${address_type}.addresses ${address} ipv4.gateway 172.31.255.1 ${address_type}.route-metric 999 wireguard.ip4-auto-default-route false`, (err, stdout, stderr) => {
-        process.exec(`nmcli connection modify ${con_name} wireguard.private-key "${private_key}"`, async (err, stdout, stderr) => {
-          let tmp_string = `[wireguard-peer.${master.manet_public_key}]\nendpoint=${master.address}:${master.address_port}\nallowed-ips=0.0.0.0/0\npersistent-keepalive=20\n`;
-          await Helper.appendFiles('/etc/NetworkManager/system-connections/', `${con_name}.nmconnection`, `\n${tmp_string}`);
-          process.exec(`nmcli connection load /etc/NetworkManager/system-connections/${con_name}.nmconnection`, (err,stdout, stderr) => {
-            process.exec(`nmcli connection up ${con_name}`, (err, stdout, stderr) => {
-              process.exec(`iptables -t nat -A POSTROUTING -o ${con_name} -j MASQUERADE`, (err, stdout, stderr) => {
-                process.exec(`netfilter-persistent save`, (err, stdout, stderr) => resolve())
-              })
-            });
+        process.exec(`nmcli connection modify ${con_name} wireguard.private-key "${private_key}"`, (err, stdout, stderr) => {
+          process.exec(`nmcli connection modify ${con_name} wireguard.mtu 1420`, async(err, stdout, stderr) => {
+            let tmp_string = `[wireguard-peer.${master.manet_public_key}]\nendpoint=${master.address}:${master.address_port}\nallowed-ips=0.0.0.0/0\npersistent-keepalive=20\n`;
+            await Helper.appendFiles('/etc/NetworkManager/system-connections/', `${con_name}.nmconnection`, `\n${tmp_string}`);
+            process.exec(`nmcli connection load /etc/NetworkManager/system-connections/${con_name}.nmconnection`, (err,stdout, stderr) => {
+              process.exec(`nmcli connection up ${con_name}`, (err, stdout, stderr) => {
+                process.exec(`iptables -t nat -A POSTROUTING -o ${con_name} -j MASQUERADE`, (err, stdout, stderr) => {
+                  process.exec(`netfilter-persistent save`, (err, stdout, stderr) => resolve())
+                })
+              });
+            })
           })
         })
       })
